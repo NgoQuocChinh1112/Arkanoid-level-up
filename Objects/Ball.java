5090c616e3f10dc71bd07b5b019de8b6fce5fa7f
Quunk <letrinhquocquan06@gmail.com>
2025-10-06 21:50:46 +0700

    fill lớp Ball và Paddle

--- Objects/Ball.java
+++ Objects/Ball.java
@@ -1,32 +1,49 @@
 package Objects;
 
-public class Ball extends MovableObject {
-    private float speed;
-    private float directionX;
-    private float directionY;
+import java.awt.*;
 
-    public Ball(float x, float y, int width, int height, float speed, float dirX, float dirY) {
-        super(x, y, width, height, dirX * speed, dirY * speed);
-        this.speed = speed;
-        this.directionX = dirX;
-        this.directionY = dirY;
+public class Ball extends MovableObject {
+    private float speed = 5f;
+    private boolean launched = false;
+
+    public Ball(float x, float y, int width, int height) {
+        super(x, y, width, height);
+        dx = 0;
+        dy = 0;
     }
 
     @Override
     public void update() {
-        // TODO
+        move();
     }
 
     @Override
-    public void render(Renderer renderer) {
-        renderer.draw(this);
+    public void render(Graphics2D g2) {
+        g2.setColor(Color.WHITE);
+        g2.fillOval(Math.round(x), Math.round(y), width, height);
+        g2.setColor(Color.GRAY);
+        g2.drawOval(Math.round(x), Math.round(y), width, height);
     }
 
-    public void bounceOff(GameObject obj) {
-        // TODO
+    public void launch(float dx, float dy) {
+        this.dx = dx;
+        this.dy = dy;
+        launched = true;
     }
 
-    public void checkCollision(GameManager gameManager) {
-        // TODO
+    public boolean isLaunched() { return launched; }
+
+    public void resetToPaddle(Paddle paddle) {
+        launched = false;
+        dx = 0; dy = 0;
+        setX(paddle.getX() + paddle.getWidth() / 2f - getWidth() / 2f);
+        setY(paddle.getY() - getHeight() - 1);
+    }
+
+    public void setSpeed(float s) { this.speed = s; }
+    public float getSpeed() { return speed; }
+
+    public Rectangle getBounds() {
+        return new Rectangle(Math.round(x), Math.round(y), width, height);
     }
 }
--- Objects/Paddle.java
+++ Objects/Paddle.java
@@ -1,43 +1,33 @@
 package Objects;
 
-import PowerUps.PowerUp;
+import java.awt.*;
 
 public class Paddle extends MovableObject {
-    private float speed;
-    private PowerUp currentPowerUp;
+    private float speed = 6f;
 
-    public Paddle(float x, float y, int width, int height, float speed) {
-        super(x, y, width, height, 0f, 0f);
-        this.speed = speed;
+    public Paddle(float x, float y, int width, int height) {
+        super(x, y, width, height);
     }
 
     @Override
     public void update() {
-        // TODO
+        move();
     }
 
     @Override
-    public void render(Renderer renderer) {
-        renderer.draw(this);
+    public void render(Graphics2D g2) {
+        g2.setColor(Color.LIGHT_GRAY);
+        g2.fillRoundRect(Math.round(x), Math.round(y), width, height, 8, 8);
+        g2.setColor(Color.DARK_GRAY);
+        g2.drawRoundRect(Math.round(x), Math.round(y), width, height, 8, 8);
     }
 
-    public void moveLeft() {
-        // TODO
-        x -= speed;
-        if (x < 0) {
-            x = 0;
-        }
-    }
+    public void moveLeft() { dx = -speed; }
+    public void moveRight() { dx = speed; }
 
-    public void moveRight() {
-        // TODO
-        x += speed;
-        if (x > width) {
-            x = width;
-        }
-    }
+    public float getSpeed() { return speed; }
+    public void setSpeed(float s) { this.speed = s; }
 
-    public void applyPowerUp(PowerUp p) {
-        // TODO
-    }
+    public void setWidth(int w) { this.width = w; }
+    public int getWidth() { return width; }
 }

db86b9be9f12ac3f65bfb6c396465045be73e3b1
NgoQuocChinh1112 <24020048@vnu.edu.vn>
2025-10-06 20:55:20 +0700

    Bổ sung GameObject

--- Objects/GameObject.java
+++ Objects/GameObject.java
@@ -16,9 +16,28 @@ public abstract class GameObject {
     public abstract void update();
     public abstract void render(Renderer renderer);
 
+    /**
+     * Trả về vùng bao của đối tượng dưới dạng hình chữ nhật.
+     * @return Hình chữ nhật đại diện cho vùng chiếm chỗ của đối tượng
+     */
+    public Rectangle getBounds() {
+        return new Rectangle(Math.round(x), Math.round(y), width, height);
+    }
+
+    /**
+     * Kiểm tra xem đối tượng này có va chạm với đối tượng khác hay không.
+     * @param other Đối tượng cần kiểm tra va chạm
+     * @return true nếu hai đối tượng giao nhau, ngược lại false
+     */
+    public boolean intersects(GameObject other) {
+        return this.getBounds().intersects(other.getBounds());
+    }
+
     // getters / setters
     public float getX() { return x; }
     public float getY() { return y; }
     public int getWidth() { return width; }
     public int getHeight() { return height; }
+    public void setX(float x) { this.x = x; }
+    public void setY(float y) { this.y = y; }
 }

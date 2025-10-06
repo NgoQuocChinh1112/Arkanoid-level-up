54247bbed1d42be1a5896ed95df69428090e67cc
NgoQuocChinh1112 <24020048@vnu.edu.vn>
2025-10-06 21:45:10 +0700

    Fill Class MovableObject

--- Objects/MovableObject.java
+++ Objects/MovableObject.java
@@ -1,16 +1,26 @@
 package Objects;
 
 public abstract class MovableObject extends GameObject {
-    protected float dx;
-    protected float dy;
+    protected float dx = 0;
+    protected float dy = 0;
 
-    public MovableObject(float x, float y, int width, int height, float dx, float dy) {
-        super(x, y, width, height);
-        this.dx = dx;
-        this.dy = dy;
+    public MovableObject(float x, float y, int width, int height) {
+        super(x,y,width,height);
     }
 
     public void move() {
-        // TODO
+        this.x += dx;
+        this.y += dy;
     }
+
+    @Override
+    public void update() {
+        move();
+    }
+
+    // getters/setters
+    public float getDx() { return dx; }
+    public float getDy() { return dy; }
+    public void setDx(float dx) { this.dx = dx; }
+    public void setDy(float dy) { this.dy = dy; }
 }
